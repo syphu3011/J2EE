927f707e877ba48b22025deae9882a4c7d6748e9
Phuong vy <vy002359@gmail.com>
2023-10-10 07:18:25 +0700

    "search","menu"

--- src/views/pages/main.tsx
+++ src/views/pages/main.tsx
@@ -8,6 +8,9 @@ import "../Styles/Responsive.css";
 import "../components/content/menuCard"
 import PageContent from '../components/content/content';
 import { BrowserRouter } from 'react-router-dom';
+import "../components/content/menuCard"
+import PageContent from '../components/content/content';
+import { BrowserRouter } from 'react-router-dom';
 export default class Main extends React.Component<any, any>   
 {  
     so() {
@@ -17,10 +20,17 @@ export default class Main extends React.Component<any, any>
         return (
             <div>
                 <BrowserRouter>
+                <BrowserRouter>
                 <Layout>
                     <Header/>
                     <PageContent/>
+                    <PageContent/>
+                    <PageContent/>
+                    <PageContent/>
+                    <PageContent/>
+                    <PageContent/>
                 </Layout>
+                </BrowserRouter>
                 </BrowserRouter>
                 
           </div>
