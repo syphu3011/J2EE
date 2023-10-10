5c4296d7c956283192008b5b116ab6b1cb0ba0cf
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-10-10 22:48:43 +0700

    Update Models

---
+++ src/models/database/models/chitietquyen.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class ChiTietQuyen extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    ChiTietQuyen.init(
+        {
+            maquyen: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                references: "Quyen",
+            },
+            // manhacungcap: {
+            //     type: DataTypes.INTEGER,
+            //     primaryKey: true,
+            //     references: "NhaCungCap",
+            // },
+        },
+        {
+            sequelize,
+            modelName: "ChiTietQuyen",
+            tableName: "ChiTietQuyen",
+            timestamps: false,
+        }
+    );
+    return ChiTietQuyen;
+};
