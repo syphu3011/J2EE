df59f379549cf5a8a9b89bebc82af7853fa23867
Menh1802 <vongcongmenh2k2@gmail.com>
2023-10-12 17:12:47 +0700

    update customer

--- src/views/admin/pages/customer.tsx
+++ src/views/admin/pages/customer.tsx
@@ -3,18 +3,20 @@ import { Form, Input, InputNumber, Popconfirm, Table, Typography } from 'antd';
 
 interface Item {
   key: string;
+  id: string;
   name: string;
   age: number;
-  address: string;
+  status: string;
 }
 
 const originData: Item[] = [];
 for (let i = 0; i < 20; i++) {
   originData.push({
     key: i.toString(),
+    id: `${i}`,
     name: `Edward ${i}`,
     age: 32,
-    address: `London Park no. ${i}`,
+    status: `Hoạt động`,
   });
 }
 interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
@@ -69,7 +71,7 @@ const Customer= () => {
   const isEditing = (record: Item) => record.key === editingKey;
 
   const edit = (record: Partial<Item> & { key: React.Key }) => {
-    form.setFieldsValue({ name: '', age: '', address: '', ...record });
+    form.setFieldsValue({id: '', name: '', age: '', status: '', ...record });
     setEditingKey(record.key);
   };
 
@@ -103,21 +105,27 @@ const Customer= () => {
 
   const columns = [
     {
-      title: 'Tên',
+      title: 'Mã',
+      dataIndex: 'id',
+      width: 'auto',
+      editable: true,
+    },
+    {
+      title: 'tên',
       dataIndex: 'name',
-      width: '25%',
+      width: 'auto',
       editable: true,
     },
     {
       title: 'Tuổi',
       dataIndex: 'age',
-      width: '15%',
+      width: 'auto',
       editable: true,
     },
     {
-      title: 'Địa chỉ',
-      dataIndex: 'address',
-      width: '40%',
+      title: 'Trạng thái',
+      dataIndex: 'status',
+      width: 'auto',
       editable: true,
     },
     {
@@ -128,15 +136,15 @@ const Customer= () => {
         return editable ? (
           <span>
             <Typography.Link onClick={() => save(record.key)} style={{ marginRight: 8 }}>
-              Save
+              Lưu
             </Typography.Link>
             <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
+              <a>Hủy</a>
             </Popconfirm>
           </span>
         ) : (
           <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
-            Edit
+            Sửa
           </Typography.Link>
         );
       },
