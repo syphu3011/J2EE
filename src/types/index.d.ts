9854a7f424a993b30ef406f32202894a85b5c4e5
syphu3011 <syphu1995@gmail.com>
2023-10-18 14:28:06 +0700

    commit all

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,7 +10,10 @@ declare module "*.jpg" {
     const path: string;
     export default path;
 }
-
+declare module "*.jpeg" {
+  const path: string;
+  export default path;
+}
 declare module "*.png" {
     const path: string;
     export default path;
--- webpack.config.js
+++ webpack.config.js
@@ -39,16 +39,9 @@ module.exports = {
            
           },
           {
-            test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+            test: /\.(jpeg|jpg|png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
             loader: "url-loader",
             options: { limit: false },
-          },
-          {
-            test: /\.(png|jpe?g|gif|jp2|webp)$/,
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-            },
           }
         ],
 
