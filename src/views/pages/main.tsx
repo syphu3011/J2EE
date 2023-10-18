f4f9d55f2a0ac80038115ecd7ec2ccbd41dce777
syphu3011 <syphu1995@gmail.com>
2023-10-18 14:29:48 +0700

    delete jpeg

--- src/views/pages/main.tsx
+++ src/views/pages/main.tsx
@@ -8,7 +8,6 @@ import "../Styles/Responsive.css";
 import "../components/content/menuCard"
 import PageContent from '../components/content/content';
 import { BrowserRouter } from 'react-router-dom';
-import img from '../components/Image/JPEG_example_flower.jpeg'
 export default class Main extends React.Component<any, any>   
 {  
     so() {
@@ -20,7 +19,6 @@ export default class Main extends React.Component<any, any>
                 <BrowserRouter>
                 <Layout>
                     <Header/>
-                    <img src={img} alt="" />
                     <PageContent/>
                 </Layout>
                 </BrowserRouter>
