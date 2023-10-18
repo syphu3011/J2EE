8134bfe94c459f31d932b932f4872c6372ea2722
syphu3011 <syphu1995@gmail.com>
2023-10-18 14:28:06 +0700

    commit all

--- src/views/pages/main.tsx
+++ src/views/pages/main.tsx
@@ -8,6 +8,7 @@ import "../Styles/Responsive.css";
 import "../components/content/menuCard"
 import PageContent from '../components/content/content';
 import { BrowserRouter } from 'react-router-dom';
+import img from '../components/Image/JPEG_example_flower.jpeg'
 export default class Main extends React.Component<any, any>   
 {  
     so() {
@@ -19,6 +20,7 @@ export default class Main extends React.Component<any, any>
                 <BrowserRouter>
                 <Layout>
                     <Header/>
+                    <img src={img} alt="" />
                     <PageContent/>
                 </Layout>
                 </BrowserRouter>
