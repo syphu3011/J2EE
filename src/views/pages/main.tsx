d43e822b045e5c097c2553d2ba80b0e7de611306
syphu3011 <syphu1995@gmail.com>
2023-10-18 16:03:57 +0700

    fix conflict

--- src/views/pages/main.tsx
+++ src/views/pages/main.tsx
@@ -8,9 +8,6 @@ import "../Styles/Responsive.css";
 import "../components/content/menuCard"
 import PageContent from '../components/content/content';
 import { BrowserRouter } from 'react-router-dom';
-import "../components/content/menuCard"
-import PageContent from '../components/content/content';
-import { BrowserRouter } from 'react-router-dom';
 export default class Main extends React.Component<any, any>   
 {  
     so() {
