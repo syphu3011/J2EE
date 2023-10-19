79e710c999e93fce62c13fef8c8a81378fb85d2d
Menh1802 <vongcongmenh2k2@gmail.com>
2023-10-19 19:21:56 +0700

    product update

--- src/views/admin/router/router.tsx
+++ src/views/admin/router/router.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import HomeAdmin from '../components/pagecontent/home';
 import Customer from '../components/pagecontent/customer';
 import {Routes,Route} from 'react-router-dom';
-import Leftbar from '../components/layout';
+import Product from '../components/pagecontent/product'
 import Receipt from '../components/pagecontent/receipt';
 
 export default class AppRoutesAdmin extends React.Component {
     
      render(){
-        const data = [ {
-            id:Customer,
-            element:<Customer/>,
-        }]
+
           return(
                <Routes>
                     <Route path="/" element={<HomeAdmin/>}   ></Route>
                     <Route path="/Customers" element={<Customer/>}></Route>
                     <Route path="/Receipt" element={<Receipt/>} ></Route>
+                    <Route path="/Products" element={<Product/>} ></Route>
                </Routes>
           )
      }
