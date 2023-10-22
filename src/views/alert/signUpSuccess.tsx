b50afaef951e1c82194a512cd536927e2b7e7a19
Phuong vy <vy002359@gmail.com>
2023-10-23 02:22:05 +0700

    changeStateAfterLogin
    a small bug of register is not fixed!

---
+++ src/views/alert/signUpSuccess.tsx
@@ -0,0 +1,51 @@
+import { Modal } from "antd";
+import React from "react";
+import Login from "../components/login/login";
+
+interface SuccessLoginState {
+  visible: boolean;
+}
+
+export default class SuccessLogin extends React.Component<{}, SuccessLoginState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+  }
+
+  componentDidMount() {
+    this.showSuccessModal();
+  }
+
+  showSuccessModal = () => {
+    Modal.success({
+      content: 'Tạo tài khoản thành công! Hãy đăng nhập để trải nghiệm',
+      onOk: this.handleOk,
+    });
+  }
+
+  handleOk = () => {
+    this.setState({ visible: false });
+  }
+
+  render() {
+    const { visible } = this.state;
+
+    return (
+      <>
+        {visible ? (
+          <Login
+            onClose={() => {
+              throw new Error("Function not implemented.");
+            }}
+            isLoggedIn={false}
+            setIsLoggedIn={(isLoggedIn) => {
+              throw new Error("Function not implemented.");
+            }}
+          />
+        ) : null}
+      </>
+    );
+  }
+}
