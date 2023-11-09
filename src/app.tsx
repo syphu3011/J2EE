7c8ff6c8b8a255f329f9d045396403129213045e
Menh1802 <vongcongmenh2k2@gmail.com>
2023-11-09 14:37:51 +0700

    back app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom";
 import Main from "./views/pages/main";
 import Admin from "./views/admin/pages/admin";
 import { CartProvider } from "react-use-cart";
-// const param = window.location.search;
-// let component_render;
-// switch (param) {
-//   case "?admin=true":
-//     component_render = <Admin />;
-//     break;
-//   default:
-//     component_render = (
-//       <CartProvider>
-//         <Main />
-//       </CartProvider>
-//     );
-//     break;
-// }
-// ReactDOM.render(component_render, document.getElementById("app"));
-ReactDOM.render(<Admin />, document.getElementById("app"));
+const param = window.location.search;
+let component_render;
+switch (param) {
+  case "?admin=true":
+    component_render = <Admin />;
+    break;
+  default:
+    component_render = (
+      <CartProvider>
+        <Main />
+      </CartProvider>
+    );
+    break;
+}
+ReactDOM.render(component_render, document.getElementById("app"));
+// ReactDOM.render(<Admin />, document.getElementById("app"));
