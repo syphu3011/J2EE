54900c0e2c736b596698d6552892347a99bcb28b
Menh1802 <vongcongmenh2k2@gmail.com>
2023-11-29 19:59:57 +0700

    a

---
+++ src/views/components/thaotest.d.ts
@@ -0,0 +1,1 @@
+export default function thaotest(): import("react/jsx-runtime").JSX.Element;
---
+++ src/views/components/thaotest.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, Flex, Tooltip } from 'antd';
+
+
+
+const thaotest =()=> {
+
+    return (
+
+                <Button type="primary" shape="circle">
+                    A
+                </Button>
+    )
+};
+export default thaotest;
