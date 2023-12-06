42d621a4572590f748fdf0590045ca9d7cb7f17e
Menh1802 <vongcongmenh2k2@gmail.com>
2023-12-06 16:18:17 +0700

    update order

--- src/views/admin/components/pagecontent/Orders/order.tsx
+++ src/views/admin/components/pagecontent/Orders/order.tsx
@@ -156,10 +156,13 @@ const Order = () => {
             <Form.Item label="Đến ngày:">
               <DatePicker defaultValue={dayjs()} format={dateFormat} />
             </Form.Item>
+            <Button
+              type="primary"
+              style={{ width: "15%", marginLeft: "20%", background: "green" }}
+            >
+              Xuất excel
+            </Button>
           </div>
-          <Button type="primary" style={{ width: "70%", marginLeft: 30 }}>
-            Xuất excel
-          </Button>
         </Header>
         <Content style={contentStyle}>
           <Form form={form} component={false}>
