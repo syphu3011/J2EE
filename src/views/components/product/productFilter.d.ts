80ef2108312d81bbe50764232fdfd3b8df79dad6
Phuong vy <vy002359@gmail.com>
2023-12-07 09:18:36 +0700

    fillter

--- src/views/components/product/productFilter.d.ts
+++ src/views/components/product/productFilter.d.ts
@@ -1,13 +1,14 @@
 import React from "react";
 interface ProductProps {
     productData: any[];
+    selectedTree: any[];
 }
 interface ProductState {
     minValue: number;
     maxValue: number;
     productData: any[];
 }
-export default class Product extends React.PureComponent<ProductProps, ProductState> {
+export default class ProductFilter extends React.PureComponent<ProductProps, ProductState> {
     constructor(props: any);
     componentDidUpdate(prevProps: ProductProps): void;
     handleChange: (value: any) => void;
