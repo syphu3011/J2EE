23f0f23e727e93efedd8863c6bf4f9379a780599
syphu3011 <syphu1995@gmail.com>
2023-12-07 17:36:07 +0700

    fix change in4

--- src/controllers/modules/customer/changeinformation.ts
+++ src/controllers/modules/customer/changeinformation.ts
@@ -18,19 +18,19 @@ export async function getinformation(){
     `
     return request(query)
 }
-export async function changeinformation(id: number, name: String, username: String, birth: String, phone_number: String){
+export async function changeinformation(id: number, name: String, birth: String, phone_number: String){
     // chuyển ngày sinh về String trước
     // dạng ngày sinh "yyyy-mm-dd"
     const query =  `
-    mutation changeinformation ($id: ID, $name: String!, $username: String!, $birth: String!, $phone_number: String!) {
-        suaThongTinCaNhan(input:{ma: $id, ten: $name, tentaikhoan: $username, ngaysinh: $birth, sodienthoai: $phone_number}) {
+    mutation changeinformation ($id: ID, $name: String!, $birth: String!, $phone_number: String!) {
+        suaThongTinCaNhan(input:{ma: $id, ten: $name, ngaysinh: $birth, sodienthoai: $phone_number}) {
             status
             message
         }
     }
     `
     const variables = {
-        id, name, birth, phone_number, username
+        id, name, birth, phone_number
     }
     return request(query, variables)
 }
